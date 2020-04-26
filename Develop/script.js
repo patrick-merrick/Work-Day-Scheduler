f6f2122e4eea5a008170bb3de94d6c086425d1b7
patrick.merrick <patrick.merrick@gmail.com>
2020-04-25 17:19:47 -0700

    commit 1

---
+++ Develop/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+function hourCheck(){
+    //css variable future past and present
+    //get the current hour and comapre to the hour for the block
+}
+hourCheck()
+setInterval(hourCheck,5000)
+});
